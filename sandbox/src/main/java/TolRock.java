518a6ed82bb2c4410e0c9443cbf78670d7d3ffb6
TolReed <tolreed@gmail.com>
2019-03-19 15:56:26 +0200

    Added a new project sandbox
    Added a new project sandbox - and run gradle for the project

---
+++ sandbox/src/main/java/TolRock.java
@@ -0,0 +1,10 @@
+public class TolRock {
+	
+	public static void main(String[] args) {
+		//TODO auto-generated (was't auto-generated, I create it by myself)
+		System.out.println("Hey, there. How it's going?");
+		System.out.println("You will be the awesome aEngineer, work, work, work! Don't forget to chill!");
+		System.out.println("Be a ROCK");
+	}
+
+}
