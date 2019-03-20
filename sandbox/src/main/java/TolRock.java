52dc44b716f084a4bfc139323ad129b70544d49d
TolReed <tolreed@gmail.com>
2019-03-20 16:24:10 +0200

    updated

--- sandbox/src/main/java/TolRock.java
+++ sandbox/src/main/java/TolRock.java
@@ -3,8 +3,8 @@ public class TolRock {
 	public static void main(String[] args) {
 		//TODO auto-generated (was't auto-generated, I create it by myself)
 		System.out.println("Hey, there. How it's going?");
-		System.out.println("You will be the awesome aEngineer, work, work, work! Don't forget to chill!");
-		System.out.println("Be a ROCK");
+		System.out.println("Quality in details, work, work, work! Don't forget to chill!");
+		System.out.println("Be the ROCK");
 	}
 
 }
