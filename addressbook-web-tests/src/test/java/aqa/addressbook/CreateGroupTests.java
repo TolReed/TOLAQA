5ba7f6b1c51d1b067825263b265bd3de3ac12e3b
TolReed <tolreed@gmail.com>
2019-06-05 19:35:51 +0300

    Create Group Tests

--- addressbook-web-tests/src/test/java/aqa/addressbook/CreateGroupTests.java
+++ addressbook-web-tests/src/test/java/aqa/addressbook/CreateGroupTests.java
@@ -1,4 +1,62 @@
 package aqa.addressbook;
 
-public class CreateGroupTests {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+    public class CreateGroupTests {
+
+        FirefoxDriver wd;
+
+    @BeforeMethod
+    public  void setUp () throws Execption {
+        wd = new FirefoxDriver;
+        wd.manage().timeouts().impliciyWait (60, SECONDS);
+        wd.get("http://localhost/addressbook/group.php");
+        login();
+    }
+
+    public void login () {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void testGroupCreation () {
+        wd.findElement(By.linkText("groups")).click();
+        wd.findElement(By.name("new")).click();
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys("TolRockGroup_AQA");
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys("TolRochGroupHEader");
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys("TolRock - AQA");
+        wd.findElement(By.name("submit")).click();
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    @AfterMethod
+    public  void tearDown () {
+        wd.quit();
+    }
+
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentExeption) {
+            return false;
+        }
+    }
+
+
+
 }
