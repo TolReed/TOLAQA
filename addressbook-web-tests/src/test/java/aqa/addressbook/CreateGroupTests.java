628ed01860cf606358a2a9bfc023113e7d84fa49
TolReed <tolreed@gmail.com>
2019-06-10 18:51:09 +0300

    change

--- addressbook-web-tests/src/test/java/aqa/addressbook/CreateGroupTests.java
+++ addressbook-web-tests/src/test/java/aqa/addressbook/CreateGroupTests.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
     public void testGroupCreation () {
         gotoGroupPage();
         initGroupCreation();
-        fillGroupData(new CreateGroupData("TolRockGroup_AQA707070" , "TolRochGroupHEader707070", "TolRock707070 - AQA"));
+        fillGroupData(new CreateGroupData("TolRockGroup_AQA6102019+777" , "TolRochGroupHEader707070", "TolRock707070 - AQA"));
         submitGroupCreation("submit");
         returnToGroupPage("group page");
 
