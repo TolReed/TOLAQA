bb1efa39cbb65f6360f0c02709289771b52e6006
TolReed <tolreed@gmail.com>
2019-06-20 20:14:42 +0300

    fix

--- addressbook-web-tests/src/test/java/aqa/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/aqa/addressbook/tests/ContactModificationTests.java
@@ -8,8 +8,10 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification () {
         app.getNavigationHelper().gotoHomePage();
-        app.getContactHelper().initContactModification(); // app. спочатку кожного метода, тому що замінили наслідування делегуваннямб і створили нове поле при рефікторингу
-        app.getContactHelper().fillContactForm(new ContactData("TolRockGroup_AQA - change" , "TolRoch - change", null), false); // null як параметер, для значення за замовчуванням
+        app.getContactHelper().initContactModification();
+        // app. спочатку кожного метода, тому що замінили наслідування делегуваннямб і створили нове поле при рефікторингу
+        app.getContactHelper().fillContactForm(new ContactData("TolRockGroup_AQA - change" , "TolRoch - change", null), false);
+        // null як параметер, для значення за замовчуванням, false так як для модифікації нема дропдаунв на сторінці цій
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
 
