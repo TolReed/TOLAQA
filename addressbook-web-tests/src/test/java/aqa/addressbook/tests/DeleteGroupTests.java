7f6e394147ebac0ce2fd3d13fd729399149c7767
TolReed <tolreed@gmail.com>
2019-06-21 19:31:16 +0300

    precondition validation - реалізована перевірка виконання передумов

--- addressbook-web-tests/src/test/java/aqa/addressbook/tests/DeleteGroupTests.java
+++ addressbook-web-tests/src/test/java/aqa/addressbook/tests/DeleteGroupTests.java
@@ -1,5 +1,6 @@
 package aqa.addressbook.tests;
 
+import aqa.addressbook.model.CreateGroupData;
 import org.testng.annotations.*;
 
 public class DeleteGroupTests extends TestBase {
@@ -7,6 +8,10 @@ public class DeleteGroupTests extends TestBase {
     @Test
     public void testDeleteGroup() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {//якщо не існує ні одної групи
+            app.getGroupHelper().createGroup(new CreateGroupData("Tol-AutoCreate", "Tol-AutoCreate", null));
+        }
+
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/aqa/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/aqa/addressbook/tests/GroupModificationTests.java
@@ -9,6 +9,9 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {//just a comment here, please change it whenever you want, thanks
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {//якщо не існує ні одної групи
+            app.getGroupHelper().createGroup(new CreateGroupData("Tol-AutoCreate", "Tol-AutoCreate", null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupData(new CreateGroupData("Change1-name", "Change2-header", "Change3-footer"));
